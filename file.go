62cb4628a6fe1c6c6477b95aa3016083da9cc279
Leo Liu <karta1041337@gmail.com>
2018-09-01 18:47:21 +0800

    Add temp file support

--- file.go
+++ file.go
@@ -101,3 +101,45 @@ func Appendln(path string, content ...interface{}) (len int, err error) {
 
 	return
 }
+
+// WriteTemp write to temp file
+func WriteTemp(pattern string, content ...interface{}) (path string, len int, err error) {
+	file, err := ioutil.TempFile("", pattern)
+	if err != nil {
+		return
+	}
+	path = file.Name()
+	defer file.Close()
+
+	len, err = fmt.Fprint(file, content...)
+
+	return
+}
+
+// WritefTemp write to temp file with format
+func WritefTemp(pattern string, format string, content ...interface{}) (path string, len int, err error) {
+	file, err := ioutil.TempFile("", pattern)
+	if err != nil {
+		return
+	}
+	path = file.Name()
+	defer file.Close()
+
+	len, err = fmt.Fprintf(file, format, content...)
+
+	return
+}
+
+// WritelnTemp write to temp file with newline
+func WritelnTemp(pattern string, content ...interface{}) (path string, len int, err error) {
+	file, err := ioutil.TempFile("", pattern)
+	if err != nil {
+		return
+	}
+	path = file.Name()
+	defer file.Close()
+
+	len, err = fmt.Fprintln(file, content...)
+
+	return
+}
