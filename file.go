037e84b79efd99bc23c0efcfbc28aa1f326bfaa7
Leo Liu <karta1041337@gmail.com>
2018-09-23 16:57:26 +0800

    Add Copy

--- file.go
+++ file.go
@@ -1,9 +1,15 @@
 package file
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
+	"unicode"
 
+	"github.com/otiai10/copy"
 	"github.com/sirupsen/logrus"
 	"leoliu.io/logger"
 )
@@ -76,4 +82,82 @@ func Read(path string) (content string, err error) {
 	}
 
 	return
+}
+
+// Copy copy files
+func Copy(dest string, src ...string) error {
+	if intLog {
+		intLogger.WithFields(
+			logger.DebugInfo(1, logrus.Fields{
+				"destination": dest,
+				"source":      src,
+			}),
+		).Debugln("Copy file . . .")
+	}
+
+	var fullFileList []string
+	var errorMsg strings.Builder
+
+	for _, val := range src {
+
+		var patternBuilder strings.Builder
+
+		for _, val := range strings.Split(val, "/") {
+			if strings.Contains(val, "*") {
+				for _, r := range val {
+					if unicode.IsLetter(r) {
+						patternBuilder.WriteString(fmt.Sprintf("[%c%c]", unicode.ToLower(r), unicode.ToUpper(r)))
+					} else {
+						patternBuilder.WriteString(string(r))
+					}
+				}
+			} else {
+				patternBuilder.WriteString(val)
+			}
+			patternBuilder.WriteString("/")
+		}
+		pattern := strings.TrimRight(patternBuilder.String(), "/")
+
+		if files, err := filepath.Glob(pattern); err != nil {
+			if intLog {
+				intLogger.WithFields(
+					logger.DebugInfo(1, logrus.Fields{
+						"pattern": pattern,
+					}),
+				).WithError(err).Warnln("Cannot find file")
+				errorMsg.WriteString("Cannot find file: ")
+				errorMsg.WriteString(pattern)
+				errorMsg.WriteString(", reason: ")
+				errorMsg.WriteString(err.Error())
+				errorMsg.WriteString("; ")
+			}
+		} else {
+			fullFileList = append(fullFileList, files...)
+		}
+	}
+
+	for _, val := range fullFileList {
+		systemsavPath := strings.Split(val, string(os.PathSeparator))
+		dest := dest + "/" + systemsavPath[len(systemsavPath)-1]
+		if err := copy.Copy(val, dest); err != nil {
+			if intLog {
+				intLogger.WithFields(
+					logger.DebugInfo(1, logrus.Fields{
+						"file_path": val,
+					}),
+				).WithError(err).Warnln("Cannot copy file")
+				errorMsg.WriteString("Cannot copy file: ")
+				errorMsg.WriteString(val)
+				errorMsg.WriteString(", reason: ")
+				errorMsg.WriteString(err.Error())
+				errorMsg.WriteString("; ")
+			}
+		}
+	}
+
+	if errorMsg.Len() == 0 {
+		return nil
+	} else {
+		return errors.New(errorMsg.String())
+	}
 }
